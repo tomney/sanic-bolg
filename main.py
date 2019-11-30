48e734234f4f180f575d80741413ee8a4de61c24
tomney <gregory@clearbanc.com>
2019-11-30 18:08:02 -0500

    finished adding middleware to build HTML from pieces

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from sanic import Sanic
-from sanic import response
-from sanic.log import logger
-from bolg import bolg
+from sanic import Sanic, Blueprint
 
+from app.articles import bp as articles_bp
+from app.images import bp as images_bp
+
+bolg = Blueprint.group(articles_bp, images_bp, url_prefix="/bolg")
 
 app = Sanic()
 app.config.from_pyfile("./config.py")
+app.static("/", "./static/main.html")
 app.blueprint(bolg)
-
-
-@app.route("/")
-async def test(request):
-    logger.info("Testing it up")
-    return response.json({"hello": "world"})
-
 
 if __name__ == "__main__":
     app.run(debug=True, access_log=True)
